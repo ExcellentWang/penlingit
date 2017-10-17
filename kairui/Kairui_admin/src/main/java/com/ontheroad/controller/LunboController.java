7d7ada71b26c64458485c19ae35bfae26ac9fa2b
胡俊 <Hujun@qbm360.com>
2017-10-17 17:04:20 +0800

    you hua

--- kairui/Kairui_admin/src/main/java/com/ontheroad/controller/LunboController.java
+++ kairui/Kairui_admin/src/main/java/com/ontheroad/controller/LunboController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import com.ontheroad.core.util.UploadUtil;
 import com.ontheroad.entity.Lunbo;
 import com.ontheroad.service.LunboService;
 import com.ontheroad.utils.MapUtil;
@@ -28,38 +29,10 @@ public class LunboController {
 	@RequestMapping("/addLunbo")
 	@ResponseBody
 	public  Map<Object, Object> add(Lunbo lunbo,@RequestParam("file") CommonsMultipartFile file,HttpServletRequest request){
-		//获取本地文件地址
-        String path = request.getSession().getServletContext().getRealPath("upload");  
-        String fileName = file.getOriginalFilename();  
-        System.out.println(path);  
-        File targetFile = new File(path, fileName);  
-        if(!targetFile.exists()){  
-            targetFile.mkdirs();  
-        }  
-        //保存  
-        try {  
-            file.transferTo(targetFile);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-        String sImg= "http://"+getRemoteHost(request)+":9023"+"/view/upload/"+fileName;
+        String sImg= UploadUtil.save(file, request);
         lunbo.setsImg(sImg);
 		lunboService.addOrUpdate(lunbo);
 		return MapUtil.getSuccessJson();
-	}
-	
-	public String getRemoteHost(javax.servlet.http.HttpServletRequest request){
-	    String ip = request.getHeader("x-forwarded-for");
-	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
-	        ip = request.getHeader("Proxy-Client-IP");
-	    }
-	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
-	        ip = request.getHeader("WL-Proxy-Client-IP");
-	    }
-	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
-	        ip = request.getRemoteAddr();
-	    }
-	    return ip.equals("0:0:0:0:0:0:0:1")?"127.0.0.1":ip;
 	}
 	
 	@RequestMapping("/getLunboList")
