09bd308316ee6015d6cb73043dd035c1a438e0bd
Administrator <Administrator@192.168.0.103>
2017-10-30 21:39:50 +0800

    用戶协议地址

--- kairui/Kairui_Common/src/main/java/com/ontheroad/pojo/user/EULA.java
+++ kairui/Kairui_Common/src/main/java/com/ontheroad/pojo/user/EULA.java
@@ -8,9 +8,43 @@ import java.io.Serializable;
 public class EULA implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Getter @Setter private int id;
-    @Getter @Setter private String title;      // 标题
-    @Getter @Setter private String content;    // 内容
-    @Getter @Setter private String createTime; // 创建时间
-    @Getter @Setter private String address;    // html地址
+     private int id;
+     private String title;      // 标题
+     private String content;    // 内容
+     private String createTime; // 创建时间
+     private String address;    // html地址
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+     
 }
