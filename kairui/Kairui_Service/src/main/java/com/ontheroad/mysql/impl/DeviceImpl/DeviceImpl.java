137f7fee315f0e9bad3442764b952718cf37b1e2
Administrator <Administrator@SC-201710220223>
2017-12-09 11:07:33 +0800

    ..

--- kairui/Kairui_Service/src/main/java/com/ontheroad/mysql/impl/DeviceImpl/DeviceImpl.java
+++ kairui/Kairui_Service/src/main/java/com/ontheroad/mysql/impl/DeviceImpl/DeviceImpl.java
@@ -2,7 +2,6 @@ package com.ontheroad.mysql.impl.DeviceImpl;
 
 import java.io.File;
 import java.net.InetSocketAddress;
-import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,7 +12,6 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 import org.apache.mina.core.future.ReadFuture;
 import org.apache.mina.core.future.WriteFuture;
 import org.apache.mina.core.session.IoSession;
@@ -90,7 +88,7 @@ public class DeviceImpl implements DeviceService {
 	private GuaranteeTypeMapper guaranteeTypeMapper;
 	
 
-	private static final Logger logger = Logger.getLogger(DeviceImpl.class);
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DeviceImpl.class);
 
 	@Override
 	public Map<Object, Object> getUserDevice(User user) {
